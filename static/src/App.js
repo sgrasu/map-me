2988ed6455fc1935dd5cda96404c62e8e83c9a42
sgrasu <sgrasu17@gmail.com>
2018-09-09 21:55:08 -0500

    use uber's react-mapbox to render map

--- static/src/App.js
+++ static/src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
-import ReactMapboxGl, { Layer, Feature } from "react-mapbox-gl";
+import ReactMapboxGl, { Layer, Feature, GeoJSONLayer } from "react-mapbox-gl";
+import * as geojson from './sample_points.json';
+import Map from './components/Map'
 
-const Map = ReactMapboxGl({
-  accessToken: 'pk.eyJ1Ijoic2dyYXN1IiwiYSI6ImNqbGxwOWV5OTB6eHgzcGxkdTRwbmF1MHoifQ.iF-dbH3wdRc0Y_mr4oGDEA'
-});
+// const Map = ReactMapboxGl({
+//   accessToken: 'pk.eyJ1Ijoic2dyYXN1IiwiYSI6ImNqbGxwOWV5OTB6eHgzcGxkdTRwbmF1MHoifQ.iF-dbH3wdRc0Y_mr4oGDEA'
+// });
 
 
 
@@ -15,19 +17,8 @@ class App extends Component {
       <div class='App col12 pad4 contain fill-navy dark clip'>
         <div className='sidebar pad2'>Listing</div>
         <div id='map' className='map pad2'>
-          <Map
-            style="mapbox://styles/mapbox/streets-v9"
-            containerStyle={{
-              height: "100vh",
-              width: "100vw"
-            }}>
-            <Layer
-              type="symbol"
-              id="marker"
-              layout={{ "icon-image": "marker-15" }}>
-              <Feature coordinates={[-0.481747846041145, 51.3233379650232]} />
-            </Layer>
-          </Map></div>
+        <Map></Map>
+        </div>
       </div>
     );
   }
